3bc887d55d417a4ae5733a030bc60f8bab83ee33
phil <zhaoxiufei@gmail.com>
2016-04-21 17:33:07 +0800

    Spring listener

---
+++ src/main/java/com/ssm/runoob/service/StartInitFactoryListener.java
@@ -0,0 +1,27 @@
+package com.ssm.runoob.service;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * Created by Phil on 2016/4/21.
+ */
+@Service
+public class StartInitFactoryListener implements ApplicationListener<ContextRefreshedEvent> {
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        if (contextRefreshedEvent.getApplicationContext().getParent() == null) {
+            logger.info("#############容器初始化以后执行###########");
+            logger.info("#############onApplicationEvent###########");
+            logger.info("#############容器初始化以后执行###########");
+            logger.info("#############onApplicationEvent###########");
+            logger.info("#############容器初始化以后执行###########");
+        }
+
+    }
+}
